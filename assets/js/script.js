663db7f3faa621be782b120d57ee6d2bcd25adea
marleyschneiderr <marley33@uw.edu>
2023-02-08 18:22:31 -0800

    Trying to start API in Javascript

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,3 +7,10 @@
 // THEN I am presented with a 5-day forecast that displays the date, an icon representation of weather conditions, the temperature, the wind speed, and the humidity
 // WHEN I click on a city in the search history
 // THEN I am again presented with current and future conditions for that city
+
+const API_KEY = "7ff1d8af886ebe90b5084ef41fcbb528";
+const API_URL = `https://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}&units=metric`;
+
+async function displayWeatherData(c)
+
+
